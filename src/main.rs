39579f1baa60a96d72c395766b197629062fa3ae
lichong <lc@molasync.com>
2016-04-29 00:44:58 +0800

    add middleware

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
 extern crate iron;
 extern crate time;
-extern crate mount;
 extern crate staticfile;
+extern crate router;
+extern crate mount;
+extern crate logger;
 
 use iron::prelude::*;
-use mount::Mount;
+use router::Router;
 use std::path;
 use staticfile::Static;
+use mount::Mount;
+use logger::Logger;
 
 mod routers;
 
 fn main() {
+    let mut router = Router::new();
+
+    router.get("/", routers::index);
+    router.post("/api", routers::api);
+
     let mut mount = Mount::new();
-    mount.mount("/", routers::index);
-    mount.mount("/api", routers::api);
-    mount.mount("/public/", Static::new(path::Path::new("target/public/")));
+    mount.mount("/", router)
+        .mount("/public/", Static::new(path::Path::new("target/public/")));
     
-    Iron::new(mount).http("localhost:3000").unwrap();
+    let (logger_before, logger_after) = Logger::new(None);
+    
+    let mut chain = Chain::new(mount);
+    chain.link_before(logger_before);
+    chain.link_after(logger_after);
+    Iron::new(chain).http("localhost:3000").unwrap();
 }
