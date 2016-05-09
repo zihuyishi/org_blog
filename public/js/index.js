0cef64a92ae40a59449def19a38de3c32f9fd047
zihuyishi <516378258@qq.com>
2016-05-09 22:47:27 +0800

    调整前端

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,17 @@
-$(document).ready(function() {
-	$.get("/api/list_blog", function(data, status) {
+$(document).ready(function () {
+	var datasource = {
+		loaded: false,
+		blogs: []
+		};
+	var content = new Vue({
+		el: '#blog_list',
+		data: datasource
+	});
+	$.get("/api/list_blog", function (data, status) {
 		if (status == "success"
-		   && data.code == 0) {
-			new Vue({
-				el: '#blog_list',
-				data: {
-					blogs: data.list
-				}
-			});
+			&& data.code == 0) {
+			content.loaded = true;
+			content.blogs = data.list;
 		}
 	});
 });
