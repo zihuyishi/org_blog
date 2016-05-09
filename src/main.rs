7f872131ff104cd1da1ec0da98a8b52198cdb821
zihuyishi <516378258@qq.com>
2016-05-09 11:57:02 +0800

    增加文件缓存

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use std::path::Path;
 use staticfile::Static;
 use mount::Mount;
 use logger::Logger;
+use std::time;
 
 mod routers;
 mod utils;
@@ -24,11 +25,13 @@ fn link_before(chain: &mut Chain) {
 }
 
 fn main() {
+    let cache_duration = time::Duration::from_secs(7*24*60*60);
+    
     let api_router = routers::api::Router::new();
     
     let mut mount = Mount::new();
-    mount.mount("/", Static::new(Path::new("public/")))
-        .mount("/blog/", Static::new(Path::new("blog/")))
+    mount.mount("/", Static::new(Path::new("public/")).cache(cache_duration))
+        .mount("/blog/", Static::new(Path::new("blog/")).cache(cache_duration))
         .mount("/api/", api_router);
     
     let mut chain = Chain::new(mount);
